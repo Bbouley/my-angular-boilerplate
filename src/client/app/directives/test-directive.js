630c7658a7521492ea3fd47c8b4925d7d3f6a39a
bbouley <b.bouley@hotmail.co.uk>
2015-11-21 15:23:47 -0700

    updating branch changes

--- src/client/app/directives/test-directive.js
+++ src/client/app/directives/test-directive.js
@@ -1,9 +1,8 @@
-angular.module('app.testDirective', []).directive('testDirective', function() {
+angular.module('app', ['ngRoute']).directive('testDirective', function() {
     return {
         restrict    : 'EA',
-        transclude  : true,
-        // templateUrl : 'app/directives/test-directive.html',
-        template : '<h1>Template instead of url</div>'
+        // transclude  : false,
+        templateUrl : 'app/directives/test-directive.html',
     }
 })
 
