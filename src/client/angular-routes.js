93d48bf448c6f18036ebdd62d30ec70b2a23ad00
bbouley <b.bouley@hotmail.co.uk>
2015-11-23 10:15:19 -0700

    completed directives refactor, now actually being called

--- src/client/angular-routes.js
+++ src/client/angular-routes.js
@@ -1,9 +1,7 @@
 (function() {
     'use strict';
 
-    var app = angular.module('app', ['ngRoute']);
-
-    app.config(['$routeProvider', function ($routeProvider) {
+    angular.module('app').config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         .when('/', {
             templateUrl : './app/partials/home.html'
--- src/client/app/directives/test-directive.js
+++ src/client/app/directives/test-directive.js
@@ -1,8 +1,11 @@
-angular.module('app', ['ngRoute']).directive('testDirective', function() {
+angular.module('app.directive').directive('testDirective', function() {
     return {
         restrict    : 'EA',
         // transclude  : false,
         templateUrl : 'app/directives/test-directive.html',
-    }
-})
+        link : function(scope) {
+            console.log(scope);
+        }
+    };
+});
 
